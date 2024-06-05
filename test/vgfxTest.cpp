75f63964bc6296c718bb195496e52d05c3cc1acf
lbroglio <lukebroglio@gmail.com>
2024-06-05 02:09:23 +0100

    Implemented tests for vec4 and vec3

--- test/vgfxTest.cpp
+++ test/vgfxTest.cpp
@@ -1,3 +1,5 @@
+#include <cmath>
+
 #include <cppunit/TestCase.h>
 #include <cppunit/TestCaller.h>
 #include <cppunit/TestSuite.h>
@@ -7,6 +9,7 @@
 
 #include "../lib/vgfx.hpp"
 #include "../lib/util.hpp"
+
 
 
 using namespace VecGFX;
@@ -246,6 +249,436 @@ class VecGFXTests : public CppUnit::TestCase{
             CPPUNIT_ASSERT_EQUAL(EXPECT, vf);
         }
 
+         /**
+         * @brief Test that the != operator works for two Vec4 type vectors
+         */
+        void testNotEqualsVec4(){
+            std::cout << "testNotEqualsVec4:\n";
+
+            // Create two Vectors to test with
+            Vec4 v1(2, 2.5, 2, 2);
+            Vec4 v2(1, 2, 1, 1);
+
+            // Assert that the result is as expected
+            std::cout << "\tExpected: 1"  << " : Actual: " << (v1 != v2) << "\n";
+            CPPUNIT_ASSERT(v1 != v2);
+
+            // Create two Vectors to test decimal values
+            Vec4 v1f(6.5, 100.1, 6, 3);
+            Vec4 v2f(6.5, 100.1, 6, 3);
+
+            // Assert that the result is as expected
+            std::cout << "\tExpected: 0" << " : Actual: " << (v1f != v2f) << "\n";
+            CPPUNIT_ASSERT(!(v1f != v2f));
+
+        }
+
+        /**
+         * @brief Test that the = operator works for Vec4 type vectors
+         */
+        void testSetVec4(){
+            std::cout << "tesSetVec4:\n";
+
+            // Create Vectors to test with
+            Vec4 EXPECT(1, 2, 3, 4);
+            Vec4 v;
+            v = EXPECT;
+
+            // Assert that the result is as expected
+            std::cout << "\tExpected: " << EXPECT << " : Actual: " << v << "\n";
+            CPPUNIT_ASSERT_EQUAL(EXPECT, v);
+        }
+
+        /**
+         * @brief Test that the magnitude method works for Vec4 type vectors
+         */
+        void testMagnitudeVec4(){
+            std::cout << "testMagnitudeVec4:\n";
+
+            // Create Vectors to test with
+            float EXPECT = 8;
+            Vec4 v(4, 4, 4, 4);
+
+            // Assert that the result is as expected
+            std::cout << "\tExpected: " << EXPECT << " : Actual: " << v.magitude() << "\n";
+            CPPUNIT_ASSERT_EQUAL(EXPECT, v.magitude());
+        }
+
+        /**
+         * @brief Test that the * operator works for Mat4 and Vec4
+         */
+        void testTransformOperatorVec4(){
+            std::cout << "testTransformOperatorVec4:\n";
+
+            // Create Vectors to test with
+            Vec4 EXPECT(6, 6, 6, 2);
+            Vec4 v(2, 2, 2, 2);
+            Mat4 m({{1, 0, 0, 2},
+                    {0, 1, 0, 2},
+                    {0, 0, 1, 2},
+                    {0, 0, 0, 1},
+                });
+            
+            Vec4 res = m * v;
+
+            // Assert that the result is as expected
+            std::cout << "\tExpected: " << EXPECT << " : Actual: " << res << "\n";
+            CPPUNIT_ASSERT_EQUAL(EXPECT, res);
+        }
+
+        /**
+         * @brief Test that the transform method works for a Vec4
+         */
+        void testTransformVec4(){
+            std::cout << "testTransformVec4:\n";
+
+            // Create Vectors to test with
+            Vec4 EXPECT(6, 6, 6, 2);
+            Vec4 v(2, 2, 2, 2);
+            Mat4 m({{1, 0, 0, 2},
+                    {0, 1, 0, 2},
+                    {0, 0, 1, 2},
+                    {0, 0, 0, 1},
+                });
+            
+           v.transform(m);
+
+            // Assert that the result is as expected
+            std::cout << "\tExpected: " << EXPECT << " : Actual: " << v << "\n";
+            CPPUNIT_ASSERT_EQUAL(EXPECT, v);
+        }
+
+       // VEC3 TESTS
+
+        /**
+         * @brief Test that finding the dot product works for two Vec3 type vectors
+         */
+        void testDotProductVec3(){
+            std::cout << "testDotProductVec3:\n";
+
+            // Create two Vectors to test with
+            Vec3 v1(2, 3, 4);
+            Vec3 v2(5, 4, 3);
+
+            // Calculate dot product of the Vectors 
+            float dotProd = v1.dot(v2);
+
+            // Assert that the dot product is as expected
+            std::cout << "\tExpected: 34.0 : Actual: " << dotProd << "\n";
+            CPPUNIT_ASSERT_EQUAL(34.0f, dotProd);
+
+            
+
+            // Create two Vectors to test decimal values
+            Vec3 v1f(1.5, 1.5, 1.5);
+            Vec3 v2f(2, 2, 2);
+
+            // Calculate dot product of the Vectors 
+            dotProd = v1f.dot(v2f);
+
+            // Assert that the dot product is as expected
+            std::cout << "\tExpected: 9.0 : Actual: " << dotProd << "\n";
+            CPPUNIT_ASSERT(compareWithEpsilon(9.0, dotProd));
+
+        }
+
+        /**
+         * @brief Test that the + operator works for two Vec3 type vectors
+         */
+        void testPlusVec3(){
+            std::cout << "testPlusVec3:\n";
+
+            // Vector to use as expected
+            Vec3 EXPECT(10, 10, 10);
+
+            // Create two Vectors to test with
+            Vec3 v1(4, 3, 9);
+            Vec3 v2(6, 7, 1);
+
+            // Calculate sum of the Vectors 
+            Vec3 sum = v1 + v2;
+
+            // Assert that the result is as expected
+            std::cout << "\tExpected: " << EXPECT << " : Actual: " << sum << "\n";
+            CPPUNIT_ASSERT_EQUAL(EXPECT, sum);
+
+            
+
+            // Create two Vectors to test decimal values
+            Vec3 v1f(2.5, 7, 5.5);
+            Vec3 v2f(7.5, 3, 4.5);
+
+            // Calculate dot product of the Vectors 
+            sum = v1f + v2f;
+
+            // Assert that the result is as expected
+            std::cout << "\tExpected: " << EXPECT << " : Actual: " << sum << "\n";
+            CPPUNIT_ASSERT_EQUAL(EXPECT, sum);
+
+        }
+
+        /**
+         * @brief Test that the += operator works for two Vec3 type vectors
+         */
+        void testPlusEqualsVec3(){
+            std::cout << "testPlusEqualsVec3:\n";
+
+            // Vector to use as expected
+            Vec3 EXPECT(10, 10, 10);
+
+            // Create two Vectors to test with
+            Vec3 v1(4, 3, 9);
+            Vec3 v2(6, 7, 1);
+
+            // Calculate sum of the Vectors 
+            v1 += v2;
+
+            // Assert that the result is as expected
+            std::cout << "\tExpected: " << EXPECT << " : Actual: " << v1 << "\n";
+            CPPUNIT_ASSERT_EQUAL(EXPECT, v1);
+
+            
+
+            // Create two Vectors to test decimal values
+            Vec3 v1f(2.5, 7, 5.5);
+            Vec3 v2f(7.5, 3, 4.5);
+
+            // Calculate dot product of the Vectors 
+            v1f += v2f;
+
+            // Assert that the result is as expected
+            std::cout << "\tExpected: " << EXPECT << " : Actual: " << v1f << "\n";
+            CPPUNIT_ASSERT_EQUAL(EXPECT, v1f);
+
+        }
+
+        /**
+         * @brief Test that the - operator works for two Vec3 type vectors
+         */
+        void testMinusVec3(){
+            std::cout << "testMinusVec3:\n";
+
+            // Vector to use as expected
+            Vec3 EXPECT;
+
+            // Create two Vectors to test with
+            Vec3 v1(5, 10, 1);
+            Vec3 v2(5, 10, 1);
+
+            // Calculate difference of the Vectors 
+            Vec3 diff = v1 - v2;
+
+            // Assert that the result is as expected
+            std::cout << "\tExpected: " << EXPECT << " : Actual: " << diff << "\n";
+            CPPUNIT_ASSERT_EQUAL(EXPECT, diff);
+
+            // Create two Vectors to test decimal values
+            Vec3 v1f(5.5, 100.1, 0.3);
+            Vec3 v2f(5.5, 100.1, 0.3);
+
+            // Calculate sum of the Vectors 
+            diff = v1f - v2f;
+
+            // Assert that the result is as expected
+            std::cout << "\tExpected: " << EXPECT << " : Actual: " << diff << "\n";
+            CPPUNIT_ASSERT_EQUAL(EXPECT, diff);
+
+        }
+
+         /**
+         * @brief Test that the - operator works for two Vec3 type vectors
+         */
+        void testMinusEqualsVec3(){
+            std::cout << "testMinusEqualsVec3:\n";
+
+            // Vector to use as expected
+            Vec3 EXPECT(5, 12, 6);
+
+            // Create two Vectors to test with
+            Vec3 v1(6, 20, 8);
+            Vec3 v2(1, 8, 2);
+
+            // Calculate difference of the Vectors 
+            v1 -= v2;
+
+            // Assert that the result is as expected
+            std::cout << "\tExpected: " << EXPECT << " : Actual: " << v1 << "\n";
+            CPPUNIT_ASSERT_EQUAL(EXPECT, v1);
+
+            // Create two Vectors to test decimal values
+            Vec3 v1f(6.5, 112, 6);
+            Vec3 v2f(1.5, 100, 0);
+
+             // Calculate difference of the Vectors 
+            v1f -= v2f;
+
+            // Assert that the result is as expected
+            std::cout << "\tExpected: " << EXPECT << " : Actual: " << v1f << "\n";
+            CPPUNIT_ASSERT_EQUAL(EXPECT, v1f);
+
+        }
+
+        /**
+         * @brief Test that the * operator works for a Vec3 and a float
+         */
+        void testScalarVec3(){
+            std::cout << "testScalarVec3:\n";
+
+            // Vector to use as expected
+            Vec3 EXPECT(10, 12, 6);
+
+            // Create two Vectors to test with
+            Vec3 v(5, 6, 3);
+
+            // Calculate difference of the Vectors 
+            Vec3 prod = 2 * v;
+
+            // Assert that the result is as expected
+            std::cout << "\tExpected: " << EXPECT << " : Actual: " << prod << "\n";
+            CPPUNIT_ASSERT_EQUAL(EXPECT, prod);
+
+            // Create two Vectors to test decimal values
+            EXPECT = Vec3(5, 11, 12);
+            Vec3 vf(2.5, 5.5, 6);
+
+             // Calculate difference of the Vectors 
+            prod = vf * 2;
+
+            // Assert that the result is as expected
+            std::cout << "\tExpected: " << EXPECT << " : Actual: " << prod << "\n";
+            CPPUNIT_ASSERT_EQUAL(EXPECT, prod);
+        }
+
+        /**
+         * @brief Test that the *= operator works for a Vec3 and a float
+         */
+        void testScalarEqualsVec3(){
+            std::cout << "testScalarEqualsVec3:\n";
+
+            // Vector to use as expected
+            Vec3 EXPECT(10, 12, 6);
+
+            // Create two Vectors to test with
+            Vec3 v(5, 6, 3);
+
+            // Calculate difference of the Vectors 
+            v *= 2;
+
+            // Assert that the result is as expected
+            std::cout << "\tExpected: " << EXPECT << " : Actual: " << v << "\n";
+            CPPUNIT_ASSERT_EQUAL(EXPECT, v);
+
+            // Create two Vectors to test decimal values
+            EXPECT = Vec3(5, 11, 12);
+            Vec3 vf(2.5, 5.5, 6);
+
+             // Calculate difference of the Vectors 
+             vf *= 2;
+
+            // Assert that the result is as expected
+            std::cout << "\tExpected: " << EXPECT << " : Actual: " << vf << "\n";
+            CPPUNIT_ASSERT_EQUAL(EXPECT, vf);
+        }
+
+         /**
+         * @brief Test that the != operator works for two Vec3 type vectors
+         */
+        void testNotEqualsVec3(){
+            std::cout << "testNotEqualsVec3:\n";
+
+            // Create two Vectors to test with
+            Vec3 v1(2, 2.5, 2);
+            Vec3 v2(1, 2, 1);
+
+            // Assert that the result is as expected
+            std::cout << "\tExpected: 1"  << " : Actual: " << (v1 != v2) << "\n";
+            CPPUNIT_ASSERT(v1 != v2);
+
+            // Create two Vectors to test decimal values
+            Vec3 v1f(6.5, 100.1, 6);
+            Vec3 v2f(6.5, 100.1, 6);
+
+            // Assert that the result is as expected
+            std::cout << "\tExpected: 0" << " : Actual: " << (v1f != v2f) << "\n";
+            CPPUNIT_ASSERT(!(v1f != v2f));
+
+        }
+
+        /**
+         * @brief Test that the = operator works for Vec3 type vectors
+         */
+        void testSetVec3(){
+            std::cout << "tesSetVec3:\n";
+
+            // Create Vectors to test with
+            Vec3 EXPECT(1, 2, 3);
+            Vec3 v;
+            v = EXPECT;
+
+            // Assert that the result is as expected
+            std::cout << "\tExpected: " << EXPECT << " : Actual: " << v << "\n";
+            CPPUNIT_ASSERT_EQUAL(EXPECT, v);
+        }
+
+        /**
+         * @brief Test that the magnitude method works for Vec3 type vectors
+         */
+        void testMagnitudeVec3(){
+            std::cout << "testMagnitudeVec3:\n";
+
+            // Create Vectors to test with
+            float EXPECT = 4 * sqrt(3);
+            Vec3 v(4, 4, 4);
+
+            // Assert that the result is as expected
+            std::cout << "\tExpected: " << EXPECT << " : Actual: " << v.magitude() << "\n";
+            CPPUNIT_ASSERT_EQUAL(EXPECT, v.magitude());
+        }
+
+        /**
+         * @brief Test that the * operator works for Mat3 and Vec3
+         */
+        void testTransformOperatorVec3(){
+            std::cout << "testTransformOperatorVec3:\n";
+
+            // Create Vectors to test with
+            Vec3 EXPECT(6, 6, 0);
+            Vec3 v(2, 2, 2);
+            Mat3 m({{1, 0, 2},
+                    {0, 1, 2},
+                    {0, 0, 0}
+                });
+            
+            Vec3 res = m * v;
+
+            // Assert that the result is as expected
+            std::cout << "\tExpected: " << EXPECT << " : Actual: " << res << "\n";
+            CPPUNIT_ASSERT_EQUAL(EXPECT, res);
+        }
+
+        /**
+         * @brief Test that the transform method works for a Vec3
+         */
+        void testTransformVec3(){
+            std::cout << "testTransformVec3:\n";
+
+            // Create Vectors to test with
+            Vec3 EXPECT(6, 6, 0);
+            Vec3 v(2, 2, 2);
+            Mat3 m({{1, 0, 2},
+                    {0, 1, 2},
+                    {0, 0, 0}
+                });
+            
+           v.transform(m);
+
+            // Assert that the result is as expected
+            std::cout << "\tExpected: " << EXPECT << " : Actual: " << v << "\n";
+            CPPUNIT_ASSERT_EQUAL(EXPECT, v);
+        }
+        
+
+        
         /**
          * @brief Build and return the a Test Suit consisting of all this classes tests
          */
@@ -258,8 +691,27 @@ class VecGFXTests : public CppUnit::TestCase{
             testSuite->addTest (new CPPUNIT_NS::TestCaller ("testConstructor", &VecGFXTests::testPlusEqualsVec4));
             testSuite->addTest (new CPPUNIT_NS::TestCaller ("testConstructor", &VecGFXTests::testMinusVec4));
             testSuite->addTest (new CPPUNIT_NS::TestCaller ("testConstructor", &VecGFXTests::testMinusEqualsVec4));
-             testSuite->addTest (new CPPUNIT_NS::TestCaller ("testConstructor", &VecGFXTests::testScalarVec4));
+            testSuite->addTest (new CPPUNIT_NS::TestCaller ("testConstructor", &VecGFXTests::testScalarVec4));
             testSuite->addTest (new CPPUNIT_NS::TestCaller ("testConstructor", &VecGFXTests::testScalarEqualsVec4));
+            testSuite->addTest (new CPPUNIT_NS::TestCaller ("testConstructor", &VecGFXTests::testNotEqualsVec4));
+            testSuite->addTest (new CPPUNIT_NS::TestCaller ("testConstructor", &VecGFXTests::testSetVec4));
+            testSuite->addTest (new CPPUNIT_NS::TestCaller ("testConstructor", &VecGFXTests::testMagnitudeVec4));
+            testSuite->addTest (new CPPUNIT_NS::TestCaller ("testConstructor", &VecGFXTests::testTransformOperatorVec4));
+            testSuite->addTest (new CPPUNIT_NS::TestCaller ("testConstructor", &VecGFXTests::testTransformVec4));
+            
+            testSuite->addTest (new CPPUNIT_NS::TestCaller ("testConstructor", &VecGFXTests::testDotProductVec3));
+            testSuite->addTest (new CPPUNIT_NS::TestCaller ("testConstructor", &VecGFXTests::testPlusVec3));
+            testSuite->addTest (new CPPUNIT_NS::TestCaller ("testConstructor", &VecGFXTests::testPlusEqualsVec3));
+            testSuite->addTest (new CPPUNIT_NS::TestCaller ("testConstructor", &VecGFXTests::testMinusVec3));
+            testSuite->addTest (new CPPUNIT_NS::TestCaller ("testConstructor", &VecGFXTests::testMinusEqualsVec3));
+            testSuite->addTest (new CPPUNIT_NS::TestCaller ("testConstructor", &VecGFXTests::testScalarVec3));
+            testSuite->addTest (new CPPUNIT_NS::TestCaller ("testConstructor", &VecGFXTests::testScalarEqualsVec3));
+            testSuite->addTest (new CPPUNIT_NS::TestCaller ("testConstructor", &VecGFXTests::testNotEqualsVec3));
+            testSuite->addTest (new CPPUNIT_NS::TestCaller ("testConstructor", &VecGFXTests::testSetVec3));
+            testSuite->addTest (new CPPUNIT_NS::TestCaller ("testConstructor", &VecGFXTests::testMagnitudeVec3));
+            testSuite->addTest (new CPPUNIT_NS::TestCaller ("testConstructor", &VecGFXTests::testTransformOperatorVec3));
+            testSuite->addTest (new CPPUNIT_NS::TestCaller ("testConstructor", &VecGFXTests::testTransformVec3));
+
 
             return testSuite;
         }
